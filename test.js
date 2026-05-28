d437b4b65f4be9aa0d96d871a7a50892817253a0
DQ-PC\Daniel <daniventf@gmail.com>
2026-05-28 14:02:53 +0100

    Add Selection and Options (basic)

--- test.js
+++
@@ -1,6 +0,0 @@
-alert("Testing stuff, get out");
-
-fetch('./test.json')
-  .then(response => response.json())
-  .then(data => alert(JSON.stringify(data)))
-  .catch(error => console.error(error));
