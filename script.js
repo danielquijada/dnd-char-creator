0f4c7eadb57617a7cbdcf9dba17615779f3f4b46
DQ-PC\Daniel <daniventf@gmail.com>
2026-05-28 17:15:05 +0100

    Add selection mechanics, still missing showing what we select in real time (and afterwards).
    Add succesive selections
    
    > Also added a few more tests in the test-selection.js to test better.

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-import { testSelection } from "./test-selection.js";
+import { testSelections } from "./test-selection.js";
 
-setSelection(testSelection);
+const selections = [];
+let currentSelection;
+
+const chosenOptions = [];
+
+testSelections.forEach(testSelection => addSelection(testSelection));
+startSelections();
+
+/*************************************************************************************************/
+
+/**
+ * Adds a new selection to the array of selections. Specifically, adds it after its master selection (the selection that made this selection necessary)
+ * so it comes right after that one.
+ */
+function addSelection(selection, masterIndex = null) {
+  if (masterIndex == null) {
+    selections.push(selection);
+  } else {
+    selections.splice(masterIndex + 1, 0, selection);
+  }
+}
+
+/**
+ * Starts the selections from the first one.
+ */
+function startSelections() {
+  currentSelection = -1;
+  nextSelection();
+}
+
+/**
+ * Sets the next selection, if any. If there isn't any left, just say so and don't show anything.
+ */
+function nextSelection() {
+  const nextSelection = selections[++currentSelection];
+  setSelection(nextSelection || { title: "No more selections, your job here is done! :)" });
+}
 
 /*************************************************************************************************/
 
@@ -22,25 +58,32 @@ function setSelection(selection) {
   const selectorBlockTitle = document.querySelector('#selector-block>#selection-title');
   selectorBlockTitle.innerText = selection.title;
 
-  // TODO add support for the "choose" parameter
+  clearOptions();
+  selection.options.forEach((opt, index) => addOption(opt.title, opt.text, index));
+}
 
-  selection.options.forEach(opt => addOption(opt.title, opt.text, opt.effects));
+/**
+ * Clears the existing options from the screen.
+ */
+function clearOptions() {
+  const optionsElement = document.querySelector('div#selector-block>div#option-list');
+  optionsElement.replaceChildren();
 }
 
 /*
  * Adds an option to the selection screen.
  */
-function addOption(title, content, effects) {
+function addOption(title, content, index) {
   const optionsElement = document.querySelector('div#selector-block>div#option-list');
 
-  optionsElement.appendChild(createOption(title, content, effects));
+  optionsElement.appendChild(createOption(title, content, index));
   processReadMoreOptions();
 }
 
 /**
  * Creates an option element, to be added in the selection screen.
  */
-function createOption(title, content, effects) {
+function createOption(title, content, index) {
   const option = document.createElement('div');
   option.classList.add('option');
 
@@ -59,7 +102,7 @@ function createOption(title, content, effects) {
   readMore.innerHTML = 'read more';
   text.appendChild(readMore);
 
-  option.onclick = () => addEffects(effects);
+  option.onclick = () => toggleOptionSelection(index);
 
   return option;
 }
@@ -81,7 +124,38 @@ function processReadMoreOptions() {
   );
 }
 
-function addEffects(effects) {
-  // TODO Add support to adding the effects in the right panel.
-  console.log('Effects:', effects);
+/**
+ * Toggles the selection for an option in the current selection
+ */
+function toggleOptionSelection(index) {
+  // TODO Remove console log.
+  console.log('Effects:', selections[currentSelection].options[index].effects);
+
+  if (chosenOptions[currentSelection] == null) {
+    chosenOptions[currentSelection] = [];
+  }
+  const chosen = chosenOptions[currentSelection];
+  if (!chosen.includes(index)) {
+    chosen.push(index);
+  } else {
+    chosen.splice(chosen.indexOf(index));
+  }
+
+  toggleSelectedClass(index);
+
+  if (selections[currentSelection].choose <= chosenOptions[currentSelection].length) {
+    nextSelection();
+  }
+
+  // TODO add "update results" or similar, to show all effects on the right panel.
 };
+
+/**
+ * Toggles the selected class for the option in the specified index.
+ */
+function toggleSelectedClass(index) {
+  const optionsElement = document.querySelector('div#selector-block>div#option-list');
+
+  optionsElement.children[index].classList.toggle('selected');
+}
+
--- test-selection.js
+++ test-selection.js
@@ -1,5 +1,5 @@
-export const testSelection = {
-  title: 'Select something while testing',
+const testSelection = {
+  title: 'Select some things while testing',
   choose: 2,
   options: [
     {
@@ -24,3 +24,130 @@ export const testSelection = {
     },
   ]
 };
+
+const secondTestSelection = {
+  title: 'Select 3 things while testing',
+  choose: 3,
+  options: [
+    {
+      title: 'Option 1',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 1"
+    },
+    {
+      title: 'Option 2',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 3',
+      text: 'Lorem ipsum, dolores de muelas amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 3"
+    },
+    {
+      title: 'Option 4',
+      text: 'Lorem ipsum, dolor at cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 4"
+    },
+    {
+      title: 'Option 5',
+      text: 'Lorem ipsum, dolor at cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 4"
+    },
+    {
+      title: 'Option 6',
+      text: 'Lorem ipsum, dolor at cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 4"
+    },
+    {
+      title: 'Option 7',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+  ]
+};
+
+const thirdTestSelection = {
+  title: 'Select only one thing while testing',
+  choose: 1,
+  options: [
+    {
+      title: 'Option 1',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 1"
+    },
+    {
+      title: 'Option 2',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 3',
+      text: 'Lorem ipsum, dolores de muelas amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 3"
+    },
+    {
+      title: 'Option 4',
+      text: 'Lorem ipsum, dolor at cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 4"
+    },
+    {
+      title: 'Option 5',
+      text: 'Lorem ipsum, dolor at cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 4"
+    },
+    {
+      title: 'Option 6',
+      text: 'Lorem ipsum, dolor at cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 4"
+    },
+    {
+      title: 'Option 7',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 8',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 9',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 10',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 11',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 12',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 13',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 14',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 15',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+  ]
+};
+
+
+export const testSelections = [testSelection, secondTestSelection, thirdTestSelection];
