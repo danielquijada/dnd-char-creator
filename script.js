925b7f9eb44505aed81e63e61a52507e7a7b0a2f
DQ-PC\Daniel <daniventf@gmail.com>
2026-05-28 15:24:26 +0100

    Add primitive selection

--- script.js
+++ script.js
@@ -1,10 +1,87 @@
-let optionTexts = Array.from(document.querySelectorAll('div.option>div.option-text'));
+import { testSelection } from "./test-selection.js";
 
-optionTexts.forEach(
-  textElement => {
-    if (textElement.scrollHeight > textElement.clientHeight) {
-      console.log('This option is clipped:', textElement);
-      textElement.classList.add('clipped');
+setSelection(testSelection);
+
+/*************************************************************************************************/
+
+/**
+ * Sets a selection for the "Selection" sreen. It expects an object formatted as follows:
+ * @param {*} selection {
+ *    title: Title of the Selection,
+ *    choose: 1, (number of choices to be made)
+ *    options: [ (array of options to be shown, each as an object as follows)
+ *      {
+ *        title: Title of the Option,
+ *        text: text to be shown in the option,
+ *        effects: effects (a string describing the effects)
+ *      }
+ *    ]
+ * }
+ */
+function setSelection(selection) {
+  const selectorBlockTitle = document.querySelector('#selector-block>#selection-title');
+  selectorBlockTitle.innerText = selection.title;
+
+  // TODO add support for the "choose" parameter
+
+  selection.options.forEach(opt => addOption(opt.title, opt.text, opt.effects));
+}
+
+/*
+ * Adds an option to the selection screen.
+ */
+function addOption(title, content, effects) {
+  const optionsElement = document.querySelector('div#selector-block>div#option-list');
+
+  optionsElement.appendChild(createOption(title, content, effects));
+  processReadMoreOptions();
+}
+
+/**
+ * Creates an option element, to be added in the selection screen.
+ */
+function createOption(title, content, effects) {
+  const option = document.createElement('div');
+  option.classList.add('option');
+
+  const header = document.createElement('h2');
+  header.classList.add('option-title');
+  header.innerHTML = title;
+  option.appendChild(header);
+
+  const text = document.createElement('div');
+  text.classList.add('option-text');
+  text.innerHTML = content;
+  option.appendChild(text);
+
+  const readMore = document.createElement('a');
+  readMore.classList.add('read-more');
+  readMore.innerHTML = 'read more';
+  text.appendChild(readMore);
+
+  option.onclick = () => addEffects(effects);
+
+  return option;
+}
+
+/**
+ * Adds a class that makes the "read more" button visible.
+ */
+function processReadMoreOptions() {
+  const optionTexts = Array.from(document.querySelectorAll('div.option>div.option-text'));
+
+  optionTexts.forEach(
+    textElement => {
+      if (textElement.scrollHeight > textElement.clientHeight) {
+        textElement.classList.add('clipped');
+      } else {
+        textElement.classList.remove('clipped');
+      }
     }
-  }
-)
+  );
+}
+
+function addEffects(effects) {
+  // TODO Add support to adding the effects in the right panel.
+  console.log('Effects:', effects);
+};
---
+++ test-selection.js
@@ -0,0 +1,26 @@
+export const testSelection = {
+  title: 'Select something while testing',
+  choose: 2,
+  options: [
+    {
+      title: 'Option 1',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 1"
+    },
+    {
+      title: 'Option 2',
+      text: 'Lorem ipsum, dolores sit amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 2"
+    },
+    {
+      title: 'Option 3',
+      text: 'Lorem ipsum, dolores de muelas amet consectetur adipisicing elit. Maxime debitis quia aut velit asperiores sed rerum unde repellat odio sequi deserunt voluptate consectetur id libero, dolor voluptatem nostrum rem. Exercitationem quos ipsa velit, minus ipsum ratione consequuntur incidunt. Tempore laboriosam laborum ratione id placeat doloribus est ut ex velit, officia incidunt eius delectus non reiciendis sed explicabo aspernatur recusandae illum quasi sapiente quisquam? Impedit alias ipsa soluta dolores? Saepe quasi inventore quisquam pariatur repudiandae? Fugiat cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 3"
+    },
+    {
+      title: 'Option 4',
+      text: 'Lorem ipsum, dolor at cumque excepturi ab quas veniam quis! Alias cupiditate, maiores ipsa neque laborum maxime distinctio, quidem repellat vitae consequatur, rerum qui aliquid! Iusto eaque omnis iste!',
+      effects: "The effects of option 4"
+    },
+  ]
+};
